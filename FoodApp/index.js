132e4f16a6232e110a2c82af17c62af5d4d37b19
Dhanashri <iamdhanashriahire@gmail.com>
2023-09-29 15:33:30 +0530

    mirchi

---
+++ FoodApp/index.js
@@ -0,0 +1,22 @@
+
+const express=require("express");
+const { dbConnection } = require("./config/db");
+
+const app=express();
+
+app.use(express.json());
+
+app.get("/",(req,res)=>{
+    res.send("homepage")
+
+})
+
+app.listen(8080,async()=>{
+    try{
+    await dbConnection
+    console.log("connected to db")
+    }catch(e){
+    console.log(e.message)
+    }
+    console.log("listening at port 8080")
+})
